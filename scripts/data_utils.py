e93dce07e42ff2a9aa8af7d2500fee28591148d6
avchavanne <128593291+avchavanne@users.noreply.github.com>
2026-05-05 15:34:17 +0200

    Update data_utils.py
    added utils for sa and fc

--- scripts/data_utils.py
+++ scripts/data_utils.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 import pandas as pd
 
-def load_data(modality='ct', variant = None, top_level_dir=None):
+def load_data(modality='ct', variant=None, model_type=None, top_level_dir=None):
     """Load and return the combined training and test datasets."""
 
     if top_level_dir is None:
@@ -33,7 +33,7 @@ def load_data(modality='ct', variant = None, top_level_dir=None):
             with open(os.path.join(root_dir,'docs','phenotypes_ct_dk_mean.txt')) as f:
                 idp_ids_dk_mean = f.read().splitlines()
             response_variables = idp_ids_dk_lh + idp_ids_dk_rh + idp_ids_dk_mean
-            out_dir = os.path.join(root_dir,'models','lifespan_ct_dk_46K_59sites')
+            out_dir = os.path.join(root_dir,'models', model_type + '_lifespan_ct_dk_46K_59sites')
         else:
             # Destrieux atlas
             df_tr = pd.read_csv(os.path.join(data_dir,'lifespan_big_controls_extended_tr.csv'), index_col=0) 
@@ -43,7 +43,7 @@ def load_data(modality='ct', variant = None, top_level_dir=None):
             with open(os.path.join(root_dir,'docs','phenotypes_ct_rh.txt')) as f:
                 idp_ids_rh = f.read().splitlines()
             response_variables = idp_ids_lh + idp_ids_rh
-            out_dir = os.path.join(root_dir,'models','lifespan_ct_67K_89sites_train')
+            out_dir = os.path.join(root_dir,'models', model_type + '_lifespan_ct_67K_89sites_train')
 
     # subcortical volumes (pu25 results)
     elif modality.lower() == 'sc':
@@ -63,20 +63,42 @@ def load_data(modality='ct', variant = None, top_level_dir=None):
             col for col in response_variables if col not in exclude
             ]
         response_variables = response_variables + ['WM-hypointensities', 'log-WM-hypointensities']
-        out_dir = os.path.join(root_dir,'models','lifespan_sc_67K_89sites')
+        out_dir = os.path.join(root_dir,'models', model_type + '_lifespan_sc_67K_89sites')
 
-    elif modality.lower() == 'sa':
+   elif modality.lower() == 'sa':
         # surface area
-        df_tr = pd.read_csv(os.path.join(data_dir,'lifespan_big_surfacearea_resample_0_tr.csv'), index_col=0) 
-        df_te = pd.read_csv(os.path.join(data_dir,'lifespan_big_surfacearea_resample_0_te.csv'), index_col=0)
-        df_tr.dropna(inplace=True,how='any')
-        df_te.dropna(inplace=True,how='any')
-        with open(os.path.join(root_dir,'docs','phenotypes_sa_lh.txt')) as f:
-            idp_ids_sa_lh = f.read().splitlines()
-        with open(os.path.join(root_dir,'docs','phenotypes_sa_rh.txt')) as f:
-            idp_ids_sa_rh = f.read().splitlines()
-        response_variables = idp_ids_sa_lh + idp_ids_sa_rh
-        out_dir = os.path.join(root_dir,'models','lifespan_sa_20K_66sites')
+        if variant and variant.upper() == 'DK':
+            # Desikan-Killiany atlas
+            df_tr = pd.read_csv(os.path.join(data_dir,'DK_lifespan_big_sa_tr.csv'), index_col=0) 
+            df_te = pd.read_csv(os.path.join(data_dir,'DK_lifespan_big_sa_te.csv'), index_col=0)
+            with open(os.path.join(root_dir,'docs','phenotypes_sa_dk_lh.txt')) as f:
+                idp_ids_sa_dk_lh = f.read().splitlines()
+            with open(os.path.join(root_dir,'docs','phenotypes_sa_dk_rh.txt')) as f:
+                idp_ids_sa_dk_rh = f.read().splitlines()
+            response_variables = idp_ids_sa_dk_lh + idp_ids_sa_dk_rh
+            out_dir = os.path.join(root_dir,'models', model_type +'_lifespan_sa_dk_46K_59sites')
+        else:
+            # Destrieux atlas
+            df_tr = pd.read_csv(os.path.join(data_dir,'lifespan_big_surfacearea_resample_0_tr.csv'), index_col=0) 
+            df_te = pd.read_csv(os.path.join(data_dir,'lifespan_big_surfacearea_resample_0_te.csv'), index_col=0)
+            df_tr.dropna(inplace=True,how='any')
+            df_te.dropna(inplace=True,how='any')
+            with open(os.path.join(root_dir,'docs','phenotypes_sa_lh.txt')) as f:
+                idp_ids_sa_lh = f.read().splitlines()
+            with open(os.path.join(root_dir,'docs','phenotypes_sa_rh.txt')) as f:
+                idp_ids_sa_rh = f.read().splitlines()
+            response_variables = idp_ids_sa_lh + idp_ids_sa_rh
+            out_dir = os.path.join(root_dir,'models', model_type +'_lifespan_sa_37K_66sites') #change number of subjects when freesurfer issue is solved
+    
+    elif modality.lower() == 'fc':
+        #functional connectome
+        df_tr = pd.read_csv(os.path.join(data_dir,'connectomes','lifespan_controls_yeo17_tr.csv')) 
+        df_te = pd.read_csv(os.path.join(data_dir,'connectomes','lifespan_controls_yeo17_te.csv'))
+        df_tr.set_index('sub_id', inplace=True)
+        df_te.set_index('sub_id', inplace=True)
+        with open(os.path.join(root_dir,'docs','phenotypes_yeo17.txt')) as f:
+            response_variables = f.read().splitlines()
+        out_dir = os.path.join(root_dir,'models', model_type +'_lifespan_fc_yeo17_21K_40sites')
 
     elif modality.lower() == 'fa':
         # diffusion (FA)
@@ -94,7 +116,7 @@ def load_data(modality='ct', variant = None, top_level_dir=None):
         with open(os.path.join(root_dir,'docs','phenotypes_fa.txt')) as f:
             idp_ids_fa = f.read().splitlines()
         response_variables = idp_ids_fa 
-        out_dir = os.path.join(root_dir,'models','lifespan_FA_24K_19sites')
+        out_dir = os.path.join(root_dir,'models', model_type + '_lifespan_FA_24K_19sites')
 
     # concatenate (split later using ptk routines)
     df = pd.concat((df_tr, df_te))
